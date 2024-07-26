ac36cb8b944a69452d0ef866ef13c3ebc4746170
Aaron Protheroe <Aaron.protheroe@sovtech.com>
2024-07-26 15:30:25 +0200

    feat(): Logo

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,10 +1,12 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Logo } from "./Logo";
 
 export function PageHeader() {
   return (
     <Flex justifyContent="space-between" mb={20}>
       <Flex alignItems="center" gap={10}>
+        <Logo />
         <Text color="gray.600">Home</Text>
         <Text color="gray.600">Products</Text>
         <Text color="gray.600">Resources</Text>
